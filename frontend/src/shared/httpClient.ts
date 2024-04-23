1eba2993f709150ee9fcdfb122b7a89757c5f272
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-04-23 08:00:51 +0100

    feat: support streaming

--- frontend/src/shared/httpClient.ts
+++ frontend/src/shared/httpClient.ts
@@ -1,22 +1,59 @@
 import axios from 'axios'
 
-type dynamic = any
+type Dynamic = any
 
-function apiUrl(url: string) {
+function buildAPIUrl(url: string) {
     return `http://localhost:8080/${url}`
 }
 
-async function post(url: string, data: dynamic) {
-    const response = await axios.post(apiUrl(url), data)
+async function post(url: string, data: Dynamic) {
+    const response = await axios.post(buildAPIUrl(url), data)
     return response.data
 }
 
 async function get(url: string) {
-    const response = await axios.get(apiUrl(url))
+    const response = await axios.get(buildAPIUrl(url))
     return response.data
+}
+
+async function* streaming(url: string, body?: any) {
+    const apiUrl = buildAPIUrl(url)
+    let fetchFn: Promise<Response>
+    if (body) {
+        fetchFn = fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    else {
+        fetchFn = fetch(apiUrl, {
+            method: 'GET'
+        })
+    }
+
+    console.log(body)
+
+    const response = await fetchFn
+    const reader = response.body?.getReader()
+    if (!reader) {
+        return
+    }
+
+    while (true!) {
+        const { done, value } = await reader.read()
+        if (done) {
+            break
+        }
+        const decoded = new TextDecoder().decode(value)
+        yield decoded
+    }
 }
 
 export default {
     post,
-    get
+    get,
+    streaming
 }
